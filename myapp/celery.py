f8d155ba6ce06d710aedb4842bc02017c61afc15
Marcos <marcos.valentee20@gmail.com>
2021-08-20 16:21:49 -0400

    pwd

--- myapp/celery.py
+++ myapp/celery.py
@@ -7,36 +7,46 @@ import uuid
 logging.basicConfig(level=logging.DEBUG)
 mylogger = logging.getLogger()
 
-# faltando desenvolvimento aqui
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('mysql://root:36546655aS!@localhost/teste')
+Session = sessionmaker(bind=engine)
+session = Session()
+
 
 class Querys():
-    def create_request(session, data):
+    def create_request(data):
         novo_pedido = CreditRequest(ticket=str(uuid.uuid4()),
         nome=data['nome'],
         idade=data['idade'],
         valor_solicitado=data['valor_solicitado'],
-        status='Processando')
+        cpf=data['CPF'],
+        status='Processando'
+        )
         session.add(novo_pedido)
         
         return novo_pedido
 
-    def consulta_request(session, _id):
-        return session.query.filter_by(id=_id).first()
+    def consulta_request(_id):
+        dados = session.query(CreditRequest).filter_by(id=_id).first()
+        return dados
+
 
 
 @celery.task()
 def async_request(data):
 
     # falta desenvolvimento aqui
-    session = db.session
     try:
-        Querys.consulta_request(session, _id=id)
+        teste = Querys.consulta_request(id)
+        print(teste.id)
         if data['idade'] < 18 or data['valor_solicitado'] > 100000000:
-            session.query(CreditRequest).filter(CreditRequest.status).update(CreditRequest.status == 'Reprovado!')
+            session.query(CreditRequest).filter(CreditRequest.id == teste._id).update({'status' :'Processado!', "resultado_validacao_pedido": "reprovado!"})
             session.commit()
-            print("SUcesso")
+            print("Reprovado!")
         else:
-            session.query(CreditRequest).filter(CreditRequest.status).update(CreditRequest.status == 'Aprovado!!')
+            session.query(CreditRequest).update({"resultado_validacao_pedido":'Aprovado!!'})
             session.commit()
             print("Sucesso!")
     except Exception as e:
